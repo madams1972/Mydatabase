369c19684d8c9c09f005042db8971bc039a919f9
fridayknight13 <73671454+madams1972@users.noreply.github.com>
2022-11-30 14:33:17 -0600

    updated the server.js file

--- connection/server.js
+++ connection/server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Scrappy2020!",
+  password: "",
   database: "employee_db",
 });
 
